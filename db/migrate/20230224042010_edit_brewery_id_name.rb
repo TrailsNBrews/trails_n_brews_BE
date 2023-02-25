bbd4bd7480c65496e8774a06f48e8783774d532f
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-02-25 14:58:38 -0500

    test/feat: happy path-serializer returning expected result;

--- db/migrate/20230224042010_edit_brewery_id_name.rb
+++
@@ -1,5 +0,0 @@
-class EditBreweryIdName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :user_breweries, :brew_id, :brewery
-  end
-end
--- spec/requests/api/v1/search_breweries_request_spec.rb
+++ spec/requests/api/v1/search_breweries_request_spec.rb
@@ -3,25 +3,35 @@ require 'rails_helper'
 describe 'Search Breweries API' do
 
   describe '#find_by_location serialized reponse' do
-    it 'render a JSON representation of all records of the requested resource' do
-      create_list(:brewery, 15)
-      count = 5
-      location = "39.7392,-104.9903" 
-      get "/api/v1/search_breweries?loc=#{location}&count=#{count}"
+    describe 'happy path-expected response' do
+      it 'returns a successful response with the expected top level and attributes keys' do 
+        count = 7
+        location = "39.7899,-105.204386" 
+        get "/api/v1/search_breweries?loc=#{location}&count=#{count}"
 
-      expect(response).to be_successful
-      expect(response.status).to eq(200)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
 
-      breweries = JSON.parse(response.body, symbolize_names: true)
-      expect(breweries[:data].count).to be <=(5)
-      
-      require 'pry';binding.pry
-      breweries.each do 
-      
+        breweries = JSON.parse(response.body, symbolize_names: true)
+        expect(breweries).to be_a(Hash)
+        expect(breweries[:data]).to be_a(Array)
+        expect(breweries[:data].count).to be <=(count)
 
+        breweries[:data].each do |brew|
+          expect(brew).to have_key(:type)
+          expect(brew).to have_key(:id)
+          expect(brew).to have_key(:attributes)
 
+          expect(brew[:attributes]).to have_key(:name)
+          expect(brew[:attributes]).to have_key(:city)
+          expect(brew[:attributes]).to have_key(:state)
+          expect(brew[:attributes]).to have_key(:postal_code)
+          expect(brew[:attributes]).to have_key(:longitude)
+          expect(brew[:attributes]).to have_key(:latitude)
+          expect(brew[:attributes]).to have_key(:website_url)
+        end
+      end
     end
-
   end
 
 end
