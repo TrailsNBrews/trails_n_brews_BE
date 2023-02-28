7347e5ff4e6098d96bf1ab90697cfeb0441b2809
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-28 09:50:19 -0700

    Adds condition to #trail_with_breweries for when there is no trail

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -1,9 +1,11 @@
 class TrailsFacade
   def self.trail_with_breweries(id)
     trail = ColoTrailService.find_by_id(id)
-    coordinates = trail[:geometry][:coordinates]
-    location = "#{coordinates[1]},#{coordinates[0]}"
-    breweries = OpenBrewService.find_by_location(location)
-    combined = {"trail": trail, "breweries": breweries}
+    if trail
+      coordinates = trail[:geometry][:coordinates]
+      location = "#{coordinates[1]},#{coordinates[0]}"
+      breweries = OpenBrewService.find_by_location(location)
+      combined = {"trail": trail, "breweries": breweries}
+    end
   end
 end
