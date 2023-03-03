e835707cdcbd98edaf5db70953585b586e17fd9a
Brandon Gray <brandonjoe88@hotmail.com>
2023-03-02 19:00:21 -0700

    feat: adds accomplishments to appropriate page

--- app/controllers/api/v1/user_breweries_controller.rb
+++ app/controllers/api/v1/user_breweries_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UserBreweriesController < ApplicationController
   def create
-    user_brewery = UserBrewery.new(user_id: params[:user], brewery_id: params[:brewery])
+    brewery = Brewery.find_by(brew_id: params[:brew_id])
+    if brewery
+      user_brewery = UserBrewery.new(user_id: params[:user_id].first, brewery_id: brewery.id)
+    else
+      Brewery.create(brew_id: params[:brew_id], name: params[:brew_name])
+      user_brewery = UserBrewery.new(user_id: params[:user_id].first, brewery_id: Brewery.last.id)
+    end
     if user_brewery.save
       render json: { "success": "User brewery added successfully" }, status: 201
     else
