6e1587616b54ae56526d17e8c3c2628fcaa78845
Brandon Gray <brandonjoe88@hotmail.com>
2023-03-02 20:17:14 -0700

    tests: fix tests for accomplishments

--- spec/requests/api/v1/user_breweries_spec.rb
+++ spec/requests/api/v1/user_breweries_spec.rb
@@ -5,8 +5,14 @@ describe 'User Breweries' do
     user = create(:user)
     brewery = create(:brewery)
 
-    post "/api/v1/user_breweries?user=#{user.id}&brewery=#{brewery.id}"
+    params = { user_id: [user.id.to_s], brew_id: brewery.brew_id, brew_name: brewery.name }
 
+    uri            = "/api/v1/user_breweries"
+    brewery_params = { user_id: params[:user_id], brew_id: params[:brew_id], brew_name: params[:brew_name] }
+    
+    post uri, params: brewery_params
+    
+    expect(response).to be_successful
     expect(response).to be_successful
 
     expect(UserBrewery.find_by(user_id: user.id,brewery_id: brewery.id)).to be_a(UserBrewery)
